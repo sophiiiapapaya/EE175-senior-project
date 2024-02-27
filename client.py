ac2d4abb5d0ebfe2805f9c668b5c377b91a117c1
Sophia W <swu241@ucr.edu>
2024-02-26 21:55:07 -0800

    update client.py

--- client.py
+++ client.py
@@ -1,41 +1,54 @@
-
 # Client.py
 import cv2
 import socket
 import pickle
 import struct
 
-#Create socket
 client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-# host_ip = '192.168.1.100'  # Change SERVER_IP to the server's IP address
-
-host_ip = '169.235.86.66'
+host_ip = '10.13.170.2'  # Change SERVER_IP to the server's IP address
 port = 9999
 
 client_socket.connect((host_ip, port))
+
 data = b""
 payload_size = struct.calcsize("Q")
 
 while True:
     while len(data) < payload_size:
-        packet = client_socket.recv(41024)  # Receiving data
-        if not packet: break
+        packet = client_socket.recv(262144*1024)  # Receiving data
+        if not packet:
+            break
         data += packet
+
+    if len(data) < payload_size:
+        continue  # Continue receiving until enough data is received
+
     packed_msg_size = data[:payload_size]
     data = data[payload_size:]
     msg_size = struct.unpack("Q", packed_msg_size)[0]
 
     while len(data) < msg_size:
-        data += client_socket.recv(41024)
+        packet = client_socket.recv(262144*1024)
+        if not packet:
+            break
+        data += packet
+
+    if len(data) < msg_size:
+        continue  # Continue receiving until complete message is received
+
     frame_data = data[:msg_size]
     data = data[msg_size:]
 
     # Deserialize frame
     frame = pickle.loads(frame_data)
+
     cv2.namedWindow("Receiving Video", cv2.WND_PROP_FULLSCREEN)
     cv2.setWindowProperty("Receiving Video", cv2.WND_PROP_FULLSCREEN, cv2.WINDOW_FULLSCREEN)
+
     cv2.imshow("Receiving Video", frame)
-    key = cv2.waitKey(1) & 0xFF
+
+    key = cv2.waitKey(25) & 0xFF
+
     if key == ord('q'):
         break
 
