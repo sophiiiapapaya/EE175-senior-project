01b6f774dcf3d62f168e79abed8b6f9b64332acf
Sophia Wu <sophiawu@ucr-secure-48-10-13-243-180.wnet.ucr.edu>
2024-03-08 19:34:20 -0800

    move outdated files

---
+++ Python files/.ipynb_checkpoints/client_receive-checkpoint.py
@@ -0,0 +1,55 @@
+# Client.py
+import cv2
+import socket
+import pickle
+import struct
+
+client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+host_ip = '10.13.170.2'  # Change SERVER_IP to the server's IP address
+port = 9999
+
+client_socket.connect((host_ip, port))
+
+data = b""
+payload_size = struct.calcsize("Q")
+
+while True:
+    while len(data) < payload_size:
+        packet = client_socket.recv(262144*1024)  # Receiving data
+        if not packet:
+            break
+        data += packet
+
+    if len(data) < payload_size:
+        continue  # Continue receiving until enough data is received
+
+    packed_msg_size = data[:payload_size]
+    data = data[payload_size:]
+    msg_size = struct.unpack("Q", packed_msg_size)[0]
+
+    while len(data) < msg_size:
+        packet = client_socket.recv(262144*1024)
+        if not packet:
+            break
+        data += packet
+
+    if len(data) < msg_size:
+        continue  # Continue receiving until complete message is received
+
+    frame_data = data[:msg_size]
+    data = data[msg_size:]
+
+    # Deserialize frame
+    frame = pickle.loads(frame_data)
+
+    cv2.namedWindow("Receiving Video", cv2.WND_PROP_FULLSCREEN)
+    cv2.setWindowProperty("Receiving Video", cv2.WND_PROP_FULLSCREEN, cv2.WINDOW_FULLSCREEN)
+
+    cv2.imshow("Receiving Video", frame)
+
+    key = cv2.waitKey(25) & 0xFF
+
+    if key == ord('q'):
+        break
+
+client_socket.close()
